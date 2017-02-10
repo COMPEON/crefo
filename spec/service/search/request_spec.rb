c264d43b112cc6667551db23b339cab69e71112a
Timo Schilling <timo@schilling.io>
2017-02-10 17:14:31 +0100

    spec search request send

--- spec/service/search/request_spec.rb
+++ spec/service/search/request_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe Crefo::Service::Search::Request, vcr: :search do
+  describe '#send' do
+    it 'sends the request' do
+      expect(subject.send).to be_truthy
+    end
+  end
+
   describe '#body' do
     it 'builds the request body' do
       expect(subject.body).to eq(
