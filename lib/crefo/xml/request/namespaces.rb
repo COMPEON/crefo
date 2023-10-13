e15e268674c4a8190b27e555a27c262507a90eee
Shefqet Lika <qeti.lika@gmail.com>
2023-10-13 13:40:59 +0200

    update crefo to latest version

--- lib/crefo/xml/request/namespaces.rb
+++ lib/crefo/xml/request/namespaces.rb
@@ -3,7 +3,7 @@ module Crefo
     class Request
       NAMESPACES = {
         'xmlns:soap' => 'http://www.w3.org/2003/05/soap-envelope',
-        'xmlns:ns' => 'https://onlineservice.creditreform.de/webservice/0600-0021'
+        'xmlns:ns' => 'https://onlineservice.creditreform.de/webservice/0710-0033'
       }.freeze
     end
   end
